6f9ca4d83d16f38dd03ba6c431fb00a7755bfab6
mshengakibet <japhetkibet@gmail.com>
2023-04-19 15:17:10 +0300

    initial commit

---
+++ crudapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Student(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = "student"
---
+++ crudapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from crudapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('views/', views.index),
+    path('index/', views.index),
+    path('show', views.show),
+    path('edit/<int:id>', views.edit),
+    path('delete/<int:id>', views.delete),
+    path('update/<int:id>', views.update),
+
+
+]
